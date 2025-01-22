902a04fb00fd29381985f4b13d5bd6a5e4c6a7da
Praduman Kumar <162427003+praduman040@users.noreply.github.com>
2025-01-22 14:40:40 +0000

    container configer done

---
+++ Practice/test3.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "pradumankumar@4255gmail.com"
+var accountPassward = "4255"
+accountCity = "Charpa"
+
+accountEmail = "pkyadav@gmail.com"
+accountPassward="505050"
+accountCity= "Delhi"
+
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassward, accountCity])
---
+++ test2.js
@@ -0,0 +1,4 @@
+const accountId = 1234
+let accountEmail = "pradumankumar@4255gmail.com"
+var accountPassward = "4255"
+accountCity = "Chapra"
