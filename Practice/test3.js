0fdbea38c82d5c846f612c41c87194533dffddd7
Praduman Kumar <162427003+praduman040@users.noreply.github.com>
2025-01-22 15:11:28 +0000

    container config done

--- Practice/test3.js
+++ Practice/test3.js
@@ -3,6 +3,8 @@ let accountEmail = "pradumankumar@4255gmail.com"
 var accountPassward = "4255"
 accountCity = "Charpa"
 
+
+
 accountEmail = "pkyadav@gmail.com"
 accountPassward="505050"
 accountCity= "Delhi"
@@ -11,3 +13,13 @@ accountCity= "Delhi"
 console.log(accountId)
 
 console.table([accountId, accountEmail, accountPassward, accountCity])
+
+
+console.log(3+5)
+
+// here are use of typeof define to the data process
+
+console.log(typeof undefined)
+console.log(typeof "Jay Shriram")
+console.log(typeof 123)
+console.log(typeof null)
