90430520e6c571d119b1b24af0fb42cbdf4cb967
Praduman Kumar <162427003+praduman040@users.noreply.github.com>
2025-01-30 13:25:50 +0000

    container config done

---
+++ Practice/test4.js
@@ -0,0 +1,19 @@
+let score =prompt("enter the marks 0 to 100: ");
+let grade;
+
+if(score>=80 && score<=100){
+    grade="A";
+}
+else if(score=60 && score <=79){
+    grade="B";
+}
+else if(score=50 && score <=59){
+    grade="C";
+
+}
+else if(score=30 && score <= 49){
+    grade="D";
+}
+else if(score=0 && score <= 29){
+    grade="F";
+}
