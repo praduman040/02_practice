08c06e56f99f7a662c2295c38171f9241362742a
Praduman Kumar <162427003+praduman040@users.noreply.github.com>
2025-01-31 13:23:23 +0000

    container config done

--- Arrays/test1.js
+++ Arrays/test1.js
@@ -1,4 +1,4 @@
-let arr=["Praduman","Anupam","Kishor","Mukesh"];
+/*let arr=["Praduman","Anupam","Kishor","Mukesh"];
 for(let i=0; i<=arr.length; i++)
 {
     console.log(arr[i]);
@@ -8,4 +8,18 @@ console.log(arr[0]);
 
 for(let hero of arr){
     console.log(hero);
-}
+}*/
+
+// Now here use of splice
+
+/*
+
+let arr=[10,20,30,40,50,60];
+arr.splice(2, 2, 101, 102);
+console.log(arr);
+*/
+
+//Now here use of slice
+
+let arr=[20,30,40,50,60,70,80,90,100];
+console.log(arr.slice(5));
