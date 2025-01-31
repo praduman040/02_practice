69ee8fc4f54ae9a54b9bbf33042419233761f3e2
Praduman Kumar <162427003+praduman040@users.noreply.github.com>
2025-01-31 11:35:29 +0000

    container config done

---
+++ Arrays/test1.js
@@ -0,0 +1,11 @@
+let arr=["Praduman","Anupam","Kishor","Mukesh"];
+for(let i=0; i<=arr.length; i++)
+{
+    console.log(arr[i]);
+    
+}
+console.log(arr[0]);
+
+for(let hero of arr){
+    console.log(hero);
+}
---
+++ Arrays/test2.js
@@ -0,0 +1,6 @@
+let arr=[10,20,30,40,50,60];
+arr.push(100);
+arr.push(90);
+arr.push(110);
+arr.pop(0);
+console.log(arr);
